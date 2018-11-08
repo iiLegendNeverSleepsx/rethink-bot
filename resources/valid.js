0896027bfb4d5ba70f45ea228bfb11fbd54de423
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-11-07 23:21:53 -0800

    Update valid.js

--- resources/valid.js
+++ resources/valid.js
@@ -6,7 +6,7 @@ exports.user = function(client, response, args) {
    if (response.guild.members.get(args[0])) {returned = ['found', response.guild.members.get(args[0])]} else
    {
      const members = response.guild.members.array();
-     const matches = members.filter(m => m.user.name.includes(args.join(" ")));
+     const matches = members.filter(m => m.user.username.includes(args.join(" ")));
      if (matches.length === 1) returned = ['found', matches[0]];
      if (matches.length === 0) returned = "none";
      if (matches.length >= 2) returned = ['multi', matches];

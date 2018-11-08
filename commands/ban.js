9b37ecf4f81c11ca3fd5ffb9afb8d74ce2ec3f2f
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-11-07 23:03:11 -0800

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -5,17 +5,22 @@ module.exports.run = async (bot, client, response, args) => {
     if(!response.member.hasPermission("BAN_MEMBERS") && !response.member.hasPermission("ADMINISTRATOR"))
       return response.reply("Insufficent permissions! You need to have the `BAN_MEMBERS` permission!");
     
-    let member = response.mentions.members.first() || response.guild.members.get(args[0]);
-    if(!member)
-      return response.reply("Please mention a valid member of this server!");
+    let member;
+    let memberf = require('../resources/valid.js').user(client, response, args);
+    if (memberf[0] = 'multi') {
+	    const embed = require('../resources/valid.js').embed(memberf[1])
+	    response.channel.send({embed});
+	    return;
+    } else {member = memberf[1]}
+	
     if(!member.kickable) 
       return response.reply("I cannot ban this user! Do they have a higher role? **Do I have ban permissions**?");
     
     let reason = args.slice(1).join(' ');
     if(!reason) reason = "No reason provided";
     
-    await member.ban(reason)
-      .catch(error => response.channel.send(`\nSorry ${response.author}, I couldn't ban because of: ${error}`));
+    //await member.ban(reason)
+    //  .catch(error => response.channel.send(`\nSorry ${response.author}, I couldn't ban because of: ${error}`));
     response.reply(`${member.user.tag} has been banned by ${response.author.tag} because: ${reason}`);
 	require('../resources/embed.js').log("BAN: " + member.tag, "Banned by " + response.author.tag + " because '**" + reason + "**'.", response);
 }

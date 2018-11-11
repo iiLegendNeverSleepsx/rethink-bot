18c02525462fd54168ea4f8c8e30a083fef6d968
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-11-11 15:15:02 -0800

    Update search.js

--- resources/search.js
+++ resources/search.js
@@ -2,10 +2,9 @@ const Discord = require('discord.js');
 
 exports.guildMember = function (term, response) {
   if (!response) return undefined;
-  const members = response.guild.members.array()
-  const matches = members.filter(member => member.user.username.includes(term))
+  const matches = response.guild.members.filter(member => member.user.username.includes(term))
   
-  if (matches.length === 0) return "null";
-  if (matches.length === 1) return matches[0];
-  if (matches.length > 1) return matches;
+  if (matches.size === 0) return null;
+  if (matches.size === 1) return matches.first();
+  return Array.from(matches.values());
 }

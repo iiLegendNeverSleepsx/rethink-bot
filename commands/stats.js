de07f6edb045e15b85d27378244e99ed39fb64c3
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-11-10 22:45:07 -0800

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -4,10 +4,12 @@ const fs = require("fs");
 module.exports.run = async (bot, client, response, args) => {
   const embed = new Discord.RichEmbed()
   .setTitle("Statistics")
-  .setDescription("**ReThink** is a public bot for different needed for everyone!")
-  .addField("Developer", client.users.get('258706134850863106').tag)
-  .addField("Other Owners", client.users.get('306287412437450753').tag + ", " + client.users.get('193979517470113792').tag)
-  .addField("Version", require('../package.json').version)
+  .setDescription("**ReThink** needs a new description.")
+  .addField("Developer", client.users.get('258706134850863106').tag, true)
+  .addField("Other Owners", client.users.get('306287412437450753').tag + ", " + client.users.get('193979517470113792').tag, true)
+  .addField("Version", require('../package.json').version, true)
+  .addField("Servers", client.guilds.size, true)
+  .addField("Users", client.users.size, true)
   .addField("Options", "~~Invite~~ (DISABLED) - [Support](https://discord.gg/gRTg6KE)");
   
   response.channel.send({embed})

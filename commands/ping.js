a122e5b13fce568b6a4bd9bd0eab83dd9317f7e3
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-11-10 16:09:55 -0800

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const fs = require("fs"); 
+
+module.exports.run = async (bot, client, response, args) => {
+  const loading = client.emojis.find("name", "loading");
+  const m = await response.channel.send(`${loading} Pinging...`);
+  
+  const embed = new Discord.RichEmbed()
+  .setDescription("Pinged!")
+  .addField("Response Time", m.createdTimestamp - message.createdTimestamp)
+  .addField("Ping", Math.round(client.ping))
+  .setColor("#ff0000")
+  
+  response.reply({embed})
+}
+
+module.exports.help = {
+  name: "ping",
+  usage: "ping",
+  description: "nil",
+  longdes: "Ping the bot.",
+  mentionedperm: "none",
+  category: "Utility"
+}

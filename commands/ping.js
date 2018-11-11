2151857b85a4f541cd803d6ef7705cc344c21672
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-11-10 16:30:04 -0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,11 +4,13 @@ const fs = require("fs");
 module.exports.run = async (bot, client, response, args) => {
   const loading = client.emojis.find(emoji => emoji.name === "loading");
   const m = await response.channel.send(`${loading} Pinging...`);
+  const times = m.createdTimestamp - response.createdTimestamp;
+  const ping = client.ping
   
   const embed = new Discord.RichEmbed()
   .setDescription("Pinged!")
-  .addField("Response Time", `${m.createdTimestamp - response.createdTimestamp * 1000} seconds`)
-  .addField("Ping", Math.round(`${client.ping * 1000} seconds`))
+  .addField("Response Time", `${times * 1000} seconds`)
+  .addField("Ping", `${Math.round(ping * 1000)} seconds`))
   .setColor("#ff0000")
   
   response.reply({embed})

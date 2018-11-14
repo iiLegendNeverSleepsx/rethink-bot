36c57b145ec37fbb4532f85c017ca19fae60d92c
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-11-13 20:35:36 -0800

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,25 @@
+module.exports.run = async (bot, client, response, args) {
+    let commands = "";
+    
+    fs.readdir("./commands", (err, files) => {
+      if (err) console.log(err);
+      let jsfile = files.filter(f => f.split(".").pop() === "js")
+      if (jsfile.length <= 0) {
+        console.log("Couldn't find any commands!")
+        return
+      }
+
+      jsfile.forEach((f, i) => {
+        let props = require(`./${f}`);
+        commands = commands + props.help.name + " - " + props.help.description + "\n";
+      });
+    });
+    
+    const embed = new Discord.RichEmbed()
+    .setTitle("All Commands")
+    .setDescription(commands)
+    .setColor("#ff0000")
+    .setFooter("Requested by YOU (" + response.author.tag + ")");
+    
+    response.author.send({embed}).then(() => response.reply("Check your direct messages!");).catch(err => response.channel.send("I was unable to send you a direct message, so here it is in chat:"); response.channel.send({embed});)
+}

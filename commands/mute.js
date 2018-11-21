4ea63bc44535fc888a475ffe22c8578169a34bb4
OmniSG <41650610+RealSGII2@users.noreply.github.com>
2018-11-20 23:48:42 -0800

    Create mute.js

---
+++ commands/mute.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const fs = require("fs"); 
+
+module.exports.run = async (bot, client, response, args) => {
+const member = response.guild.members.get( args[0].replace("<@", "").replace(">", "") );
+if (!member) return response.reply("not a valid user!");
+if (!response.author.hasPermission("MANAGE_MEMBERS") return response.reply("it doesn't seem like you can use that!");
+
+  const loading = client.emojis.find(emoji => emoji.name === "loading");
+  
+  const embed = new Discord.RichEmbed().setTitle("Muting")
+  .setDescription(loading + " Creating Muted role...");
+  
+  const m = await response.channel.send({embed}):
+  
+  if (!response.guild.roles.find("name","Muted")) {response.guild.createRole({name: "Muted"})}
+  
+  embed.setDescription(loading + " Forcing mute...");
+  
+  m.edit({embed});
+  
+  response.guild.channels.forEach(c => c.overwritePermissions(response.guild.roles.find("name", "Muted"), {'SEND_MESSAGES': false, 'ADD_REACTIONS': false})
+  
+  embed.setDescription(loading + "Muting member...")
+  
+  m.edit({embed});
+  
+  args.shift(); let reason = args.join(" ");
+  
+  member.addRole(response.guild.roles.find("name", "Muted"))
+  
+  require('../resources/embed.js').log("Moderation Action - MUTE", `**User:** ${member.tag} \n**Moderator:** ${response.author.tag} \n**Reason:** ${reason}`);
+}
+
+module.exports.help = {
+  name: "mute",
+  usage: "mute <user> [reason]",
+  description: "mute a user from text channels",
+  longdes: "soontm",
+  mentionedperm: "none",
+  category: "Testing"
+}
